6db0241fa807f4a2e203b722a3067774e37f45ec
Olivia <oliviatmanalastas@gmail.com>
2023-02-13 13:38:03 -0500

    Added styling to landing page

--- client/src/shared/Home/LandingPage.js
+++ client/src/shared/Home/LandingPage.js
@@ -5,8 +5,10 @@ const LandingPage = () => {
    return (
       <div className="landing-page">
          <div className="landing-page-banner">
-            <h1> DASH </h1>
+            <h1 className="lp-main-tag"> DASH </h1>
          </div>
+         <div className="landing-page-div-two">
+            <h1> What is Dash?</h1>
          <div className="lp-second-page">
             {/* <span> <h1> What is Dash? </h1></span> */}
                <div className="lp-item-1">
@@ -21,6 +23,8 @@ const LandingPage = () => {
                   <h1> Item 3 </h1>
                   <p> Item 3 description </p>
                </div>
+         </div>
+
          </div>
       </div>
    )

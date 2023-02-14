a80f3081c04016c21031c5cfa5c8edea72a2759f
Olivia <oliviatmanalastas@gmail.com>
2023-02-14 13:41:55 -0500

    Added dashboard component

---
+++ client/src/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Dashboard.css'
+
+const Dashboard = () => {
+   return(
+      <div>
+         <h1> Dash Board Landing Page</h1>
+      </div>
+   )
+}
+
+export default Dashboard;

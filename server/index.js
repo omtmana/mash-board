7bf633bc0d9fa7cf3d7b75478ce4d1ef1dd97392
Olivia <oliviatmanalastas@gmail.com>
2023-02-14 16:35:06 -0500

    Created index file and added server code

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// Packages / Modules
+const express = require('express')
+const app = express()
+
+// Routes
+
+
+// Start server
+const port = 4000
+app.listen(port, () => {
+   console.log(`Server running on port: ${port}`)
+})
